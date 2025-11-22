301bcb631c3ebf98bf7ef7fcd153668e069cf89a
chaimachahed04 <149796191+adambenfradj@users.noreply.github.com>
2025-11-22 09:50:36 -0500

    valider

---
+++ tp-3-chaima-chahed/scripts/snail.gd
@@ -0,0 +1,22 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+signal player_died
+const SPEED = 100.0
+var direction = -1.0
+
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func _process(delta: float) -> void:
+	position.x += direction * SPEED * delta
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player": 
+		emit_signal("player_died", body)
